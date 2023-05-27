d87964dc4665af8e78b092c4fef5ee0446081bfd
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-05-27 16:36:52 +0900

    feat: removed '

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function Home() {
 return ( 
   <>
-    <div className={styles.tracker}> Tracker box (should maybe be it's own component) </div>
+    <div className={styles.tracker}> Tracker box (should maybe be its own component)</div>
 
     <div className={styles.tracker}> Monthly breakdown</div>
 

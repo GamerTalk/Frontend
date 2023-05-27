8f3e7ed10bd39d5c0a3b53d3837ce479d131beec
LakGillJPN <lakgill88@gmail.com>
2023-05-27 14:54:37 +0900

    feat: change background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
 return ( 
   <div className={styles.container}>
-    <h1>Japan Finance hi</h1>
+    <h1>Japan Finance</h1>
   </div>
 )
 }

e27d7a797e4d51d83cc9815c42541bb7a4938144
LakGillJPN <lakgill88@gmail.com>
2023-05-27 15:24:07 +0900

    feat: add limit div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import styles from './page.module.css';
 export default function Home() {
 return ( 
   <div className={styles.container}>
-    <h1>Japan Finance</h1>
+    <h1>Place header here</h1>
+
+    <div className={styles.limit}>
+      hi
+    </div>
   </div>
 )
 }

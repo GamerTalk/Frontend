6763ee0b05f188e6c0c4b00d929dea9731c3ec16
LakGillJPN <lakgill88@gmail.com>
2023-05-30 22:00:50 +0900

    fix: add proxy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+'use client'
+
 import styles from "./page.module.css";
+import axios from 'axios'
+import React from 'react'
+import { useState, useEffect } from "react";
 
 export default function Home() {
+
+  const [name, setName] = useState([]);
+
+  useEffect(() => {
+    async function getNames() {
+      const names : any = await axios.get('/api/test/').then((result) => result.data)
+      setName(names)
+    }
+    getNames()
+    console.log('NAME', name)
+  },[name])
+
+
+
 return ( 
   <>
     <div> PLACEHOLDER  </div>

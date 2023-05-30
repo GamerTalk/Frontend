1b2a91832993db8da1b8148d921a956e0b60e2af
Lak Gill <LakGill88@gmail.com>
2023-05-30 20:06:25 +0900

    fix: remove mentions of Japan Finance (#10)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
 return ( 
   <>
-    <div className={styles.tracker}> Tracker box (should maybe be its own component)</div>
-
-    <div className={styles.tracker}> Monthly breakdown</div>
-
-    <div className={styles.tracker}> bills</div>
-
-
-
-
+    <div> PLACEHOLDER  </div>
   </>
 )
 }

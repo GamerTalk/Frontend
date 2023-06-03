fc8c7073bd2f6bb5851733a4054af1a6d92bc821
TaigaShiga <gaitatg97@gmail.com>
2023-06-03 02:41:21 +0200

    feat: styling elements side by side

--- src/app/components/layouts/UserCard.tsx
+++ src/app/components/layouts/UserCard.tsx
@@ -1,38 +1,60 @@
 "use client"
 
 import styles from "./UserCard.module.css";
-import {data, UserData} from "../../test/user"
+import {data, Person} from "../../test/user"
 import { useEffect, useState } from "react";
 
 const UserCard = () => { 
-  const [user, setUserData] = useState<UserData[]>([]);
+
+  
+  const [user, setUserData] = useState<Person[]>([]);
 
   // test data 
   const userInfoFetch = async () => { 
-
-    setTimeout(() => { 
+    setTimeout(() => {
       setUserData(data);
-    })
-
+    }, 2000)
   }
 
   useEffect(() => { 
-    
+    userInfoFetch();
   },[]);
 
   return (
     <>
-      <div className={styles.userCard}>
-        <div className={styles.userName}>
-          <p>userName</p>
+      {console.log(user)}
+      <div className={styles.userCard} onClick={()=>console.log("押されたよ")}>
+
+        <div className={styles.userInfo}>
+
+          <div className={styles.userImg}>
+            <img src="https://cdn2.thecatapi.com/images/MjA1OTMwMA.jpg" id={styles.image} />
+          </div>
+          
+          <div className={styles.userAbout}>
+            <div className={styles.text}>
+                <p>Emma</p>
+            </div>
+            <div className={styles.text}>
+                <p>Speaks: En</p>
+            </div>
+            <div className={styles.text}>
+              <div>
+                <p>Learning: JP</p>
+              </div>
+              {/* <div>ゲージ</div> */}
+            </div>
+          </div>
+
+          <div className={styles.systems}>
+            <div className={styles.game}>
+
+            </div>
+          </div>
+
         </div>
-        <div className={styles.userFluent}>
-          <p>fluent</p>
-        </div>
-        <div className={styles.userLearning}>
-          <p>userName</p>
-        </div>
-      </div>
+       
+      </div> 
     </>
   )
 }

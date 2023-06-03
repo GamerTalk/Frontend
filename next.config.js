6fbb0a6d5b2b7ed0c8f3e7dc70001f8dd68e12ca
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-03 11:53:39 +0900

    feat: proxy issues aborted

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:8000/api/:path*' // Proxy to Backend
+        }
+      ]
+    }
+  }
+
+// module.exports = nextConfig

614fef0c1aa01b113f36a63fc2b9b854a15f8320
TaigaShiga <gaitatg97@gmail.com>
2023-06-03 09:13:10 +0200

    feat: user information cards are listed and shown to the home page

--- src/app/components/layouts/UserCard.tsx
+++ src/app/components/layouts/UserCard.tsx
@@ -3,13 +3,18 @@
 import styles from "./UserCard.module.css";
 import {data, Person} from "../../test/user"
 import { useEffect, useState } from "react";
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlaystation , faXbox, faSteam} from "@fortawesome/free-brands-svg-icons";
-const UserCard = () => { 
+import { faPlaystation, faXbox, faSteam } from "@fortawesome/free-brands-svg-icons";
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
 
+const UserCard = () => {   
+  const [userData, setUserData] = useState<Person[]>([]);
+
+  interface Systems { 
+    [key: string]: IconDefinition;
+  }
   
-  const [user, setUserData] = useState<Person[]>([]);
-
   // test data 
   const userInfoFetch = async () => { 
     setTimeout(() => {
@@ -21,64 +26,62 @@ const UserCard = () => {
     userInfoFetch();
   },[]);
 
+  const systems:Systems = {
+    PC: faSteam,
+    Xbox: faXbox,
+    PlayStation: faPlaystation,
+    Switch: faGamepad
+  }
+  
   return (
     <>
-      <div className={styles.userCard} onClick={()=>console.log("押されたよ")}>
-
+      {
+      userData.map((user, index) => { 
+        return (
+        <div className={styles.userCard} onClick={() => console.log("hello")} key={index}>
         <div className={styles.userInfo}>
-
           <div className={styles.userImg}>
             <img src="https://cdn2.thecatapi.com/images/MjA1OTMwMA.jpg" id={styles.image} />
           </div>
-          
           <div className={styles.userAbout}>
             <div className={styles.text}>
-                <p>Emma</p>
+                <p>{user.name}</p>
+            </div>
+              <div className={styles.text}>
+                <p>speaks:</p>
+                {user.fluent.map((language,index) => { 
+                  return <p key={index}>{language}</p>;
+                 })}
             </div>
             <div className={styles.text}>
-                <p>Speaks:En</p>
-            </div>
-            <div className={styles.text}>
-              <div>
-                <p>Learning:JP Level1</p>
+                <div>
+                    {user.learning.map((learn, index) => {   
+                      return <p key={index}>{learn.language} : level {learn.level}</p>
+                     }
+                  )}
               </div>
-              
             </div>
           </div>
 
           <div className={styles.systems}>
             <div className={styles.gamesWrapper}>
-            <div className={styles.game}>
-              <FontAwesomeIcon
-                icon={faPlaystation}
-                className={styles.game} 
+                {user.systems.map((system,index) => { 
+                  return (
+                  <div className={styles.game} key={index}>
+                    <FontAwesomeIcon
+                    icon={systems[system]}
+                  className={styles.game} 
                 />
               </div>
-
-              <div className={styles.game}>
-              <FontAwesomeIcon
-                icon={faPlaystation}
-                className={styles.game} 
-                />
-              </div>
-              <div className={styles.game}>
-              <FontAwesomeIcon
-                icon={faPlaystation}
-                className={styles.game} 
-                />
-              </div>
-              <div className={styles.game}>
-              <FontAwesomeIcon
-                icon={faPlaystation}
-                className={styles.game} 
-                />
-              </div>
+                 )
+              })}
             </div>
           </div>
-
         </div>
-       
-      </div> 
+        </div> 
+        )
+      })
+    }
     </>
   )
 }

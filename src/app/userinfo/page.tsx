b94cfcce6796eae327077568fd011920385333c4
LakGillJPN <lakgill88@gmail.com>
2023-06-03 12:27:44 +0900

    initial commit

---
+++ src/app/userinfo/page.tsx
@@ -0,0 +1,17 @@
+
+export default function UserInfo() {
+  return (
+    <>
+    <h1>Welcome!</h1>
+    <p>Tell us a little about yourself so people can find you</p>
+
+    <form>
+      <label htmlFor="Username"> Username: </label>
+      <input type="text" id="UserName" name="Username"></input>
+
+    </form>
+       
+
+    </>
+  )
+}

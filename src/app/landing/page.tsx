a72cb6a5b1fc572a7cffa0acfccf26e4e6a108f2
LakGillJPN <lakgill88@gmail.com>
2023-06-10 15:35:27 +0900

    fix: remove the header from the landing page

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { UserAuth } from '../context/AuthContext';
+
+export default function Landing() {
+
+
+
+  return (
+    <>
+    <h1>GamerTalk</h1>
+    <h2>Sign In</h2>
+    </>
+  );
+}
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import './globals.css'
 import { Inter, Offside } from 'next/font/google'
 import Header from './components/layouts/Header'
+import Landing from './landing/page'
+import { useRouter } from "next/router";
 import { AuthContextProvider } from './context/AuthContext'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { usePathname } from 'next/navigation';
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,19 +19,24 @@ export const metadata = {
   description: 'Learn languages with games!',
 }
 
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
+ 
 }) {
+  const pathname = usePathname()
   return (
     <html lang="en">
-     
       <body className={offside.className}>
 
+      
+      
       <AuthContextProvider>
-        
-      <Header />
+      
+      {pathname === "/landing" ? "" :<Header />}
       
       {children}
       </AuthContextProvider>
@@ -35,3 +45,4 @@ export default function RootLayout({
     </html>
   )
 }
+

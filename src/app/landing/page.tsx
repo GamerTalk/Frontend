cf713d3b1e72f7ef4b1a3487557336b75a755812
LakGillJPN <lakgill88@gmail.com>
2023-06-11 00:03:26 +0900

    fix: change the landing css page to a module page

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
-import './landing.css'
+import styles from './landing.module.css';
 
 import { UserAuth } from '../context/AuthContext';
 
 export default function Landing() {
+  const renderCircleElements = () => {
+    const circleElements = [];
 
+    for (let i = 1; i <= 10; i++) {
+      const animationDelay = i % 2 === 0 ? `${i * 2}s` : `${i}s`;
+      const animationDuration = i % 2 === 0 ? `${i * 6}s` : `${i * 3}s`;
 
+      const circleStyle = {
+        animationDelay,
+        animationDuration,
+      };
+
+      circleElements.push(<li key={i} style={circleStyle}></li>);
+    }
+
+    return circleElements;
+  };
 
   return (
-    <div>
-    <h1 className='title'>GamerTalk</h1>
-    <h2>Sign In</h2> 
+    <div className={styles.area}>
+      <div className={styles.circles}>
+        <ul>{renderCircleElements()}</ul>
+      </div>
+      <div className={styles.context}>
+        <div className={styles.box}>
+          <h1 className={styles.title}>GamerTalk</h1>
+          <h2 className={styles.subtitle}>Learn a Language Through Gaming</h2>
+        </div>
+        <h2>Sign In</h2>
+        <form>
+          <input></input>
+        </form>
+      </div>
     </div>
   );
 }
-

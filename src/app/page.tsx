5749400f52c531321169ce7af19c6f3856bb6aa5
TaigaShiga <gaitatg97@gmail.com>
2023-06-13 14:49:28 +0200

    feat: remove code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,13 @@
 'use client'
 
-import axios from 'axios'
-import React from 'react'
-import { useState, useEffect } from "react";
-import UserCard from "./components/layouts/UserCard";
-import FilterArea from "./components/layouts/FilterArea";
-import {User} from "./global.t"
-import Landing from './landing/page';
+import React, { useEffect } from "react";
 import { UserAuth } from './context/AuthContext';
-import { useRouter, redirect } from 'next/navigation';
-
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
 
-  const [users, setUsers] = useState<User[]>([]);
-  const [filterWords, setFilterWords] = useState<string[]>([]);
-  const [isShowUserCard, setShowUserCard] = useState(true);
-
   const {uid} = UserAuth()
   const router = useRouter()
-
-  const fetchAllusers = async () => { 
-    try { 
-      const response = await axios.get("http://127.0.0.1:8000/api/filter-users/");
-      const allUsers: User[] = response.data;
-      setUsers(allUsers);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    if (filterWords.length === 0 && users.length === 0) {
-      fetchAllusers();
-    }
-  },[filterWords])
 
   useEffect(() => {
     if (!uid) {
@@ -51,15 +24,7 @@ export default function Home() {
   if (uid) {
     return (
       <>
-        <div>
-          <FilterArea
-            setUsers={setUsers}
-            setFilterWords={setFilterWords}
-            filterWords={filterWords}
-            setShowUserCard={setShowUserCard}
-          />
-          {isShowUserCard && <UserCard users={users} />}
-        </div>
+      <div>home</div>
       </>
     );
   }

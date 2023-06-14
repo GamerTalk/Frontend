b7967bf1060deb520333b810a7358a7711465182
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-14 21:59:03 +0900

    feat: added all icons from Font Awesome

--- src/app/components/layouts/Footer.tsx
+++ src/app/components/layouts/Footer.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 import styles from "./Footer.module.css";
 import { UserAuth } from "../../context/AuthContext";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faMagnifyingGlass, faMessage, faUser } from "@fortawesome/free-solid-svg-icons";
+faUser
 export default function Footer() {
   const { user } = UserAuth();
 
   return (
     <div className={styles.footerDiv}>
-      <h1>I am a footer</h1>
+      <FontAwesomeIcon icon={faHome} className={styles.footerIcon} />
+      <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.footerIcon} />
+      <FontAwesomeIcon icon={faMessage} className={styles.footerIcon} />
+      <FontAwesomeIcon icon={faUser} className={styles.footerIcon} />
     </div>
   );
 }

e525b9e5c663dc436aa221b5057021d380b70883
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-15 11:39:49 +0900

    feat: added links to footer icons

--- src/app/components/layouts/Footer.tsx
+++ src/app/components/layouts/Footer.tsx
@@ -2,17 +2,42 @@ import Link from "next/link";
 import styles from "./Footer.module.css";
 import { UserAuth } from "../../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faMagnifyingGlass, faMessage, faUser } from "@fortawesome/free-solid-svg-icons";
-faUser
+import {
+  faHome,
+  faMagnifyingGlass,
+  faMessage,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+faUser;
 export default function Footer() {
   const { user } = UserAuth();
 
   return (
     <div className={styles.footerDiv}>
-      <FontAwesomeIcon icon={faHome} className={styles.footerIcon} />
-      <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.footerIcon} />
-      <FontAwesomeIcon icon={faMessage} className={styles.footerIcon} />
-      <FontAwesomeIcon icon={faUser} className={styles.footerIcon} />
+      <div className={styles.footerIconDiv}>
+        <Link href='/'>
+          <FontAwesomeIcon icon={faHome} className={styles.footerIcon} />
+          <p>Home</p>
+
+        </Link>
+      </div>
+      <div className={styles.footerIconDiv}>
+        <Link href='/search'>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className={styles.footerIcon}
+          />
+          <p>Search</p>
+        </Link>
+      </div>
+      <div className={styles.footerIconDiv}>
+        <FontAwesomeIcon icon={faMessage} className={styles.footerIcon} />
+        <p>Messages</p>
+      </div>
+      <div className={styles.footerIconDiv}>
+        <FontAwesomeIcon icon={faUser} className={styles.footerIcon} />
+        <p>Profile</p>
+      </div>
     </div>
   );
 }

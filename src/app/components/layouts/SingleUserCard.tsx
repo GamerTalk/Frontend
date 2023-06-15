3f1bee9e1505266d664a766d1e7df371085502d2
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-15 21:48:04 +0900

    feat: added data type for other users pages

--- src/app/components/layouts/SingleUserCard.tsx
+++ src/app/components/layouts/SingleUserCard.tsx
@@ -8,10 +8,10 @@ import langCall from "@/app/utils/langCheckFunc";
 import { useRouter } from "next/navigation";
 import TitleCase from "@/app/utils/TitleCase";
 import Upper from "@/app/utils/Upper";
+import { OtherUsers } from "@/app/global.t";
 
-export default function SingleUserCard(props: any) {
-  const { userObject } = props
-  console.log(userObject)
+export default function SingleUserCard(userObject: OtherUsers) {
+
   const { uid } = UserAuth();
 
   return (
@@ -23,37 +23,30 @@ export default function SingleUserCard(props: any) {
           <p>{userObject.username}</p>
 
           <p className={styles.heading}>Region:</p>
-          <p>{userObject.region}</p>
+          <p>{TitleCase(userObject.user_region)}</p>
 
-          <div className={styles.language}>
-            
-          </div>
+
 
           <p className={styles.heading}>What language(s) are you fluent in?:</p>
           <p className={styles.subheading}>Check all that apply</p>
-          
 
           <p className={styles.heading}>Date of Birth: </p>
           <p> {userObject.date_of_birth}</p>
 
           <p className={styles.heading}>User Systems:</p>
           <div className={styles.language}>
+            <p className={styles.heading}>
+              What language(s) do you want to learn?
+            </p>
+            <p className={styles.subheading}>
+              1: Beginner, 2: Intermediate, 3: Advanced
+            </p>
 
-          <p className={styles.heading}>
-            What language(s) do you want to learn?
-          </p>
-          <p className={styles.subheading}>
-            1: Beginner, 2: Intermediate, 3: Advanced
-          </p>
-          
+            <p className={styles.heading}>Genre:</p>
 
-          <p className={styles.heading}>Genre:</p>
-          
+            <p className={styles.heading}>About Me:</p>
 
-          <p className={styles.heading}>About Me:</p>
-          
-
-          <p className={styles.heading}>Currently Playing:</p>
+            <p className={styles.heading}>Currently Playing:</p>
           </div>
         </>
       ) : (

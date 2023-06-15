086aa7a22f13567f6cbd0eb4a95235cba34bebd4
LakGillJPN <lakgill88@gmail.com>
2023-06-15 20:06:36 +0900

    fix: reset the textarea after a user has posted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,17 @@ import { UserAuth } from './context/AuthContext';
 import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
 import axios from "axios";
+import { Old_Standard_TT } from "next/font/google";
 
 export default function Home() {
   const [message, setMessage] = useState<string>("")
   const [posts, setPosts] = useState([])
+  const [counter, setCounter] = useState<number>(0)
 
 
   const {uid, userInfo} = UserAuth()
   const router = useRouter()
+  
 
   const handleFormSubmit = (event: { preventDefault: () => void }) => {
   event.preventDefault();
@@ -27,13 +30,15 @@ export default function Home() {
 
   axios.post('http://127.0.0.1:8000/api/new-post/', payload)
   .then(response => {
-    console.log('success!')
-    router.push('/')
+    setCounter(counter+1)
+    setMessage("")
   })
   .catch(error => {
     console.log(error);
   });
 }
+
+
 
   const handleMessage = (event: { target: { value: string; }; }) => {
   const { value } = event.target;
@@ -66,13 +71,12 @@ export default function Home() {
     }
     getData()
     console.log('saved')
-  },[uid])
+  },[uid, counter])
+
+ 
 
   useEffect(() => {
     console.log(posts)
-    //x.message - message
-    //x.time_of_message = date
-    //x.sender_data.username = username
   },[posts])
 
 
@@ -80,13 +84,13 @@ export default function Home() {
     return (
       <div style={{background: '#F0F2F5'}}>
       <form onSubmit={handleFormSubmit}>
-      <textarea rows={5} cols={40} onChange={handleMessage} className={styles.textarea} placeholder="What's going on?"/> 
+      <textarea rows={5} cols={40} onChange={handleMessage} className={styles.textarea} placeholder="What's going on?" value={message}/> 
       <div><button className={styles.button}>Post</button></div>
 
     
       </form>
 
-
+       
       {posts.map((x: any) => {
 
         const messageDate = new Date(x.time_of_message);
@@ -94,7 +98,7 @@ export default function Home() {
         const formattedDate = messageDate.toLocaleDateString(); // Change the date format as desired
         const formattedTime = messageDate.toLocaleTimeString(); // Change the time format as desired
 
-        return <div className={styles.post}>
+        return <div className={styles.post} key={x.id}>
         <div className={styles.pic}>Picture</div>
         <div className={styles.time}>{formattedDate} {formattedTime}</div>
         <div className={styles.user}>{x.sender_data.username}</div>

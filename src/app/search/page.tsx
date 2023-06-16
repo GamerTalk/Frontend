2e418f80b46826e4b27f5b16e503774339e275bf
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-16 15:19:33 +0900

    fix: moved the looping of userCard to the page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { User } from "../global.t";
 import UserCard from "../components/layouts/UserCard";
@@ -8,48 +8,48 @@ import axios from "axios";
 import SingleUserCard from "../components/layouts/SingleUserCard";
 import { OtherUsers } from "../global.t";
 
-export default function Search(){
-  
+export default function Search() {
   const [users, setUsers] = useState<User[]>([]);
   const [filterWords, setFilterWords] = useState<string[]>([]);
   const [isShowUserCard, setShowUserCard] = useState(true);
 
-  const fetchAllusers = async () => { 
-    try { 
-      const response = await axios.get("http://127.0.0.1:8000/api/filter-users/");
+  const fetchAllusers = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/filter-users/"
+      );
       const allUsers: User[] = response.data;
       setUsers(allUsers);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     if (filterWords.length === 0 && users.length === 0) {
       fetchAllusers();
     }
-  }, [filterWords])
-  
+  }, [filterWords]);
+
   return (
     <>
-       <div>
-          <FilterArea
-            setUsers={setUsers}
-            setFilterWords={setFilterWords}
-            filterWords={filterWords}
-            setShowUserCard={setShowUserCard}
-          />
-          {isShowUserCard && 
+      <div>
+        <FilterArea
+          setUsers={setUsers}
+          setFilterWords={setFilterWords}
+          filterWords={filterWords}
+          setShowUserCard={setShowUserCard}
+        />
+        {isShowUserCard && (
           <div>
-            {users[0] ?
-            <SingleUserCard userObject={users[0]} /> :
-            ""
-          }
-            <UserCard users={users} />
-
+            {users[0] ? <SingleUserCard userObject={users[0]} /> : ""}
+            {users.map((user:User, index:number) => {
+              return <UserCard user={user} key={index} />
+            })}
+            {/* <UserCard users={users} /> */}
           </div>
-            }
-        </div> 
+        )}
+      </div>
     </>
-  )
+  );
 }

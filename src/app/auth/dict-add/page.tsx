b010faf9a6573992dfc0238716f805c53e76d91d
LakGillJPN <lakgill88@gmail.com>
2023-06-20 15:26:49 +0900

    feat: make axios call to GET and POST data

--- src/app/auth/dict-add/page.tsx
+++ src/app/auth/dict-add/page.tsx
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './add.module.css';
+import axios from 'axios';
+import { UserAuth } from '@/app/context/AuthContext';
+
+
 
 interface Param {
   onClose: any
 }
 
 const Add: React.FC<Param> = ({ onClose}) => {
+  const [front, setFront] = useState("");
+  const [back, setBack] = useState("");
+
+  const {uid} = UserAuth();
+
+
+  const handleFront = (event: { target: { value: string; }; }) => {
+    const { value } = event.target;
+    setFront(value);
+  }
+
+  const handleBack = (event: { target: { value: string; }; }) => {
+    const { value } = event.target;
+    setBack(value);
+  }
+
+  const handleFormSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    const payload = {
+      user_uid: uid,
+      front,
+      back
+    }
+  
+    axios.post('http://127.0.0.1:8000/api/new-flashcard/', payload)
+      .then(response => {
+      console.log('success!')
+    })
+      .catch(error => {
+      console.log(error);
+    });
+  }
+
+
+
+  
+
+
   return (
     <div className={styles.popup}>
       <div className={styles.content}>
-        <h2>Popup Content</h2>
-        <p>This is the content of the popup.</p>
+        <h2>Add a new card</h2>
+        <form onSubmit={handleFormSubmit}>
+        <div className={styles.card}><p>Front</p><input className={styles.input} onChange={handleFront}/></div>
+        <div className={styles.card}><p>Back</p><input className={styles.input} onChange={handleBack}/></div>
+        <button>Submit</button>
+        </form>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
--- src/app/dict/page.tsx
+++ src/app/dict/page.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import styles from './dict.module.css'
 import DictCheckbox from '../components/elements/Dict-Checkbox'
+import Add from '../auth/dict-add/page'
+import axios from 'axios'
+import { UserAuth } from '../context/AuthContext'
 
 export default function Dict() {
-  return (
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [cards, setCards] = useState<any>([]);
+
+  const {uid} = UserAuth();
+
+  const config = {
+    method: 'GET',
+    headers: {
+      'uid' : uid
+    }
+  }
+
+  const url = "http://localhost:8000/api/get-flashcards/"
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        if (uid) { 
+        const userData : any  = await axios.get(url, config).then((result) => result.data)
+        setCards(userData) 
+        } else {
+          console.log('fail')
+        }
+      }
+      catch(error) {
+       console.log(error)
+      }
+    }
+    getData()
+  },[uid])
+
+  useEffect(() => {
+    console.log('CAAAAARDs', cards.map((card : any) => card.front))
+  },[cards])
+
+
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+  
+  return  cards.length > 0 ? (
     <div className={styles.body}>
       <h1>Dictionary</h1>
 
@@ -14,15 +64,18 @@ export default function Dict() {
         <p  className={styles.back}> Back</p>
       </div>
 
-        <DictCheckbox front="English" back="Japanese"/>
-        <DictCheckbox front="Word 1" back="Word 2"/>
-        <DictCheckbox front="Word 3" back="Word 4"/>
-      
+      {cards.map((card: any) => (
+        <DictCheckbox front={card.front} back={card.back} key={card.id} />
+      ))}
+    
       </form>
 
-
+      <div>
+      <button onClick={openPopup}>Open Popup</button>
+      {isPopupOpen && <Add onClose={closePopup} />}
+     </div>
 
     </div>
-  )
+  )  : "Now Loading "
 }
 

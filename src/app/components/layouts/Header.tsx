3c8faacfec371cab4f2d023d75b5a0839e09acb1
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-06-22 21:11:03 +0900

    feat: removed user image when creating user

--- src/app/components/layouts/Header.tsx
+++ src/app/components/layouts/Header.tsx
@@ -1,33 +1,33 @@
-"use client"
+"use client";
 
-import Head from "next/head" // for title
-import Link from "next/link" // link
-import styles from "./Header.module.css"
-import { UserAuth } from '../../context/AuthContext'
-import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
-import { MessagesContext } from "@/app/context/MessageContext"
-import { useContext, useEffect, useState } from "react"
+import Head from "next/head"; // for title
+import Link from "next/link"; // link
+import styles from "./Header.module.css";
+import { UserAuth } from "../../context/AuthContext";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { MessagesContext } from "@/app/context/MessageContext";
+import { useContext, useEffect, useState } from "react";
 
-export default function Header() { 
+export default function Header() {
   const { userName, userProfileURL } = useContext(MessagesContext);
-  
+
   console.log(MessagesContext);
-  
+
   const { user, logOut, userEmail, userInfo } = UserAuth();
-  
+
   const router = useRouter();
   const pathName = usePathname();
 
-  // checking current endpoint has id or something 
+  // checking current endpoint has id or something
   const messagePath = /^\/messages\/\w+$/;
   // if current endpoint is message page, header will be changed for messagePage
   const isMessagesPage = messagePath.test(pathName);
-  
-  const handleLogOut = async() => {
+
+  const handleLogOut = async () => {
     try {
       await logOut();
-      router.push('/')
+      router.push("/");
     } catch (err) {
       console.error(err);
     }
@@ -35,11 +35,11 @@ export default function Header() {
 
   return (
     <>
-    {isMessagesPage ? (
+      {isMessagesPage ? (
         <div className={styles.messageHeader}>
           <div>
             <Link href={"/messages/"}>
-            <button>Back</button>
+              <button>Back</button>
             </Link>
           </div>
           <div className={styles.userInfoForMessage}>
@@ -51,36 +51,50 @@ export default function Header() {
             </div>
           </div>
         </div>
-      ) :
-    <div>
-      <Head>
-        <title>GamerTalk</title>
-      </Head>
-      <header className={styles.headerContainer}>
-      {/* link tag to home "/" */}
-        <div className={styles.headerTitleContainer}>
-         <Link href="/home">
-          <p id={styles.headerName}> GamerTalk </p>
-        </Link>
-        </div>
-        <div className={styles.userInfo}>
-          <div className={styles.imageContainer}>
-            <img id={styles.image} src={userInfo?.profile_picture_url} alt="user-photo" />
-          </div>
+      ) : (
+        <div>
+          <Head>
+            <title>GamerTalk</title>
+          </Head>
+          <header className={styles.headerContainer}>
+            {/* link tag to home "/" */}
+            <div className={styles.headerTitleContainer}>
+              <Link href="/home">
+                <p id={styles.headerName}> GamerTalk </p>
+              </Link>
+            </div>
+            <div className={styles.userInfo}>
+              <div className={styles.imageContainer}>
+                {userInfo?.profile_picture_url ? (
+                  <img
+                    id={styles.image}
+                    src={userInfo?.profile_picture_url}
+                    alt="user-photo"
+                  />
+                ) : (
+                  ""
+                )}
+              </div>
 
-        <div className="username">
-         { userEmail ? (
-        <div className="username">
-         { userEmail ? (
-        <Link href="" onClick={handleLogOut}>Log Out</Link>
-        ) : <Link href="/auth/signin">Log In</Link> }
-         </div>
-        ) : <Link href="/auth/signin">Log In</Link> }
-         </div>
+              <div className="username">
+                {userEmail ? (
+                  <div className="username">
+                    {userEmail ? (
+                      <Link href="" onClick={handleLogOut}>
+                        Log Out
+                      </Link>
+                    ) : (
+                      <Link href="/auth/signin">Log In</Link>
+                    )}
+                  </div>
+                ) : (
+                  <Link href="/auth/signin">Log In</Link>
+                )}
+              </div>
+            </div>
+          </header>
         </div>
-      </header>
-        </div>
-      }
+      )}
     </>
-    )
-} 
+  );
+}

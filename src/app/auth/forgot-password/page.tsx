ec99cdfad060ddc012897dd1aa752a82a16ec2e9
Taylor Crouch <96517792+TaylorC19@users.noreply.github.com>
2023-07-19 20:00:36 +0900

    115 add reset password endpoint during sign in (#116)
    * feat: added reset logic to AuthContext
    
    * feat: added endpoint for reset password
    
    * feat: reset password page completed
    
    * removed logs

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { UserAuth } from "@/app/context/AuthContext";
+import { useState } from "react";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState<string>("");
+  const { resetPasswordEmail } = UserAuth();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      resetPasswordEmail(email);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Send reset request</button>
+      </form>
+    </>
+  );
+}

19fc34305ff404312d414d3a45e5914e27ada71e
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-12-07 11:21:32 +0900

    removed unused css file

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React, { useState } from 'react';
-import styles from './reset.module.css'
 import { EmailAuthProvider, reauthenticateWithCredential, updatePassword } from 'firebase/auth';
 import { UserAuth } from "../context/AuthContext";
 

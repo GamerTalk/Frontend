50d6d808c78e0de875a722a70d4bf884a6226c63
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-12-14 10:18:44 +0900

    feat: converted scale to tailwind

--- src/app/components/elements/Scale.tsx
+++ src/app/components/elements/Scale.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from "./Scale.module.css";
 
 interface Prop { 
   level:number
@@ -11,8 +10,8 @@ const Scale = (props: Prop) => {
   const backgroundColor =
   level <= 2 ? '#1E90FF' : level <= 4 ? '#FFD700' : '#00FF00';
   return (
-    <div className={styles.scaleWrapper}>
-      <div className={styles.scale} style={{ width: scaleWidth, background: backgroundColor}}></div>
+    <div className="rounded-xl m-0 w-12 bg-white border-1 border-black">
+      <div className="w-[20%] h-2.5 rounded-xl" style={{ width: scaleWidth, background: backgroundColor}}></div>
     </div>
   )
 }
